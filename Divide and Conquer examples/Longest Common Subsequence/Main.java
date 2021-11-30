40497bb03f5e037d0117498e63dc6617281efbe9
Vidhi Shah <61038939+woahvidhi@users.noreply.github.com>
2021-11-30 12:44:04 +0530

    Create Main.java

---
+++ Divide and Conquer examples/Longest Common Subsequence/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+    System.out.println(lcs.findLength("elephant","erepat"));
+    // lcs = eepat, count = 5
+  }
+}
